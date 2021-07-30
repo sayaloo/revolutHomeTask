32bcf45b5c060d847177df14b2c10d6bd1649af4
Saeed <saeeid.ehsani@gmail.com>
2021-07-30 16:05:19 +0430

    QueryClient added

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={new QueryClient()}>
+        <App/>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
