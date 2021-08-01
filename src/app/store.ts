9acef6051141d005217aab36a22d770c58b2a6e1
Saeed <saeeid.ehsani@gmail.com>
2021-08-01 21:09:27 +0430

    Updated Store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,16 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+
+import currencyReducer from '../features/currency/currencySlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    currency: currencyReducer,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
   RootState,
   unknown,
-  Action<string>
->;
+  Action<string>>;
