052004d4831d0b32cd7d0e9b09c6dc1059cb8185
Saeed <saeeid.ehsani@gmail.com>
2021-08-01 21:07:21 +0430

    updated Currency redux Slice

--- src/features/currency/currencySlice.ts
+++ src/features/currency/currencySlice.ts
@@ -1,14 +1,20 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface CurrencyState {
-  currencies: { sign: string, balance: number, htmlCode: string }[];
+  currencies: { symbol: string, balance: number, sign: string }[];
+  currencyFrom: string;
+  currencyTo: string;
+  transactionAmount: { from: string, to: string }
 }
 
 const initialState: CurrencyState = {
   currencies: [
-    { sign: 'EUR', balance: 2593.27, htmlCode: '&#8364;' },
-    { sign: 'USD', balance: 125.27, htmlCode: '&#36;' },
-    { sign: 'GBP', balance: 1523.31, htmlCode: '&#163;' } ],
+    { symbol: 'EUR', balance: 2593.27, sign: '€' },
+    { symbol: 'USD', balance: 125.27, sign: '$' },
+    { symbol: 'GBP', balance: 1523.31, sign: '£' } ],
+  currencyFrom: 'EUR',
+  currencyTo: 'USD',
+  transactionAmount: { from: '0', to: '0' },
 };
 
 
@@ -16,21 +22,37 @@ export const currencySlice = createSlice({
   name: 'currency',
   initialState,
   reducers: {
-    incrementCurrency: (state, action: PayloadAction<{ sign: string, amount: number }>) => {
-      const currency = state.currencies.find((currency) => currency.sign === action.payload.sign);
-      if (currency) {
-        currency.balance += action.payload.amount;
-      }
-    },
-    decrementCurrency: (state, action: PayloadAction<{ sign: string, amount: number }>) => {
-      const currency = state.currencies.find((currency) => currency.sign === action.payload.sign);
+    decrementAmount: (state, action: PayloadAction<{ sign: string, amount: number }>) => {
+      const currency = state.currencies.find((currency) => currency.symbol === action.payload.sign);
       if (currency) {
         currency.balance -= action.payload.amount;
       }
     },
+    incrementAmount: (state, action: PayloadAction<{ sign: string, amount: number }>) => {
+      const currency = state.currencies.find((currency) => currency.symbol === action.payload.sign);
+      if (currency) {
+        currency.balance += action.payload.amount;
+      }
+    },
+    setTransactionAmount: (state, action: PayloadAction<{ from: string, to: string }>) => {
+      state.transactionAmount.from = action.payload.from;
+      state.transactionAmount.to = action.payload.to;
+    },
+    setCurrencyFrom: (state, action: PayloadAction<string>) => {
+      state.currencyFrom = action.payload;
+    },
+    setCurrencyTo: (state, action: PayloadAction<string>) => {
+      state.currencyTo = action.payload;
+    },
   },
 });
 
-export const { incrementCurrency, decrementCurrency } = currencySlice.actions;
+export const {
+  decrementAmount,
+  incrementAmount,
+  setCurrencyFrom,
+  setCurrencyTo,
+  setTransactionAmount,
+} = currencySlice.actions;
 
 export default currencySlice.reducer;
